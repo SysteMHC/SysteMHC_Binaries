5096fa10175dcfafcec2d83ad2addf68dd044df4
wolski <wewolski@gmail.com>
2016-01-04 11:28:35 +0100

    added tests

--- testNetMHC.sh
+++ testNetMHC.sh
@@ -14,3 +14,30 @@
 ./pickpocket/pickpocket-1.1/PickPocket -p ./pickpocket/pickpocket-1.1/test/test.pep > ./pickpocket/pickpocket-1.1/test/test.pep.myout
 ./pickpocket/pickpocket-1.1/PickPocket ./pickpocket/pickpocket-1.1/test/test.fsa > ./pickpocket/pickpocket-1.1/test/test.fsa.myout
 ./pickpocket/pickpocket-1.1/PickPocket -hlaseq ./pickpocket/pickpocket-1.1/test/B0702.fsa -p ./pickpocket/pickpocket-1.1/test/test.pep > ./pickpocket/pickpocket-1.1/test/test.pep_userMHC.myout
+
+
+
+# testing cons installation
+./netMHCcons/netMHCcons-1.1/netMHCcons -inptype 1 -f ./netMHCcons/netMHCcons-1.1/test/test.pep > ./netMHCcons/netMHCcons-1.1/test/test.pep.myout
+./netMHCcons/netMHCcons-1.1/netMHCcons -f ./netMHCcons/netMHCcons-1.1/test/test.fsa > ./netMHCcons/netMHCcons-1.1/test/test.fsa.myout
+./netMHCcons/netMHCcons-1.1/netMHCcons -hlaseq ./netMHCcons/netMHCcons-1.1/test/B0702.fsa -inptype 1 -f ./netMHCcons/netMHCcons-1.1/test/test.pep > ./netMHCcons/netMHCcons-1.1/test/test.pep_userMHC.myout
+
+./netMHCcons/netMHCcons-1.1/netMHCcons -method NetMHC -inptype 1 -f ./netMHCcons/netMHCcons-1.1/test/test.pep > ./netMHCcons/netMHCcons-1.1/test/test.pep.myout.NetMHC
+./netMHCcons/netMHCcons-1.1/netMHCcons -method NetMHC -f ./netMHCcons/netMHCcons-1.1/test/test.fsa > ./netMHCcons/netMHCcons-1.1/test/test.fsa.myout.NetMHC
+./netMHCcons/netMHCcons-1.1/netMHCcons -method NetMHC -hlaseq ./netMHCcons/netMHCcons-1.1/test/B0702.fsa -inptype 1 -f ./netMHCcons/netMHCcons-1.1/test/test.pep > ./netMHCcons/netMHCcons-1.1/test/test.pep_userMHC.myout.NetMHC
+
+./netMHCcons/netMHCcons-1.1/netMHCcons -method NetMHCpan -inptype 1 -f ./netMHCcons/netMHCcons-1.1/test/test.pep > ./netMHCcons/netMHCcons-1.1/test/test.pep.myout.NetMHCpan
+./netMHCcons/netMHCcons-1.1/netMHCcons -method NetMHCpan -f ./netMHCcons/netMHCcons-1.1/test/test.fsa > ./netMHCcons/netMHCcons-1.1/test/test.fsa.myout.NetMHCpan
+./netMHCcons/netMHCcons-1.1/netMHCcons -method NetMHCpan -hlaseq ./netMHCcons/netMHCcons-1.1/test/B0702.fsa -inptype 1 -f ./netMHCcons/netMHCcons-1.1/test/test.pep > ./netMHCcons/netMHCcons-1.1/test/test.pep_userMHC.myout.NetMHCpan
+
+./netMHCcons/netMHCcons-1.1/netMHCcons -method PickPocket -inptype 1 -f ./netMHCcons/netMHCcons-1.1/test/test.pep > ./netMHCcons/netMHCcons-1.1/test/test.pep.myout.PickPocket
+./netMHCcons/netMHCcons-1.1/netMHCcons -method PickPocket -f ./netMHCcons/netMHCcons-1.1/test/test.fsa > ./netMHCcons/netMHCcons-1.1/test/test.fsa.myout.PickPocket
+
+
+# finally remove all myout files
+find . -name "*.myout*" -exec rm -rf {} \;
+./netMHCcons/netMHCcons-1.1/netMHCcons -method PickPocket -hlaseq ./netMHCcons/netMHCcons-1.1/test/B0702.fsa -inptype 1 -f ./netMHCcons/netMHCcons-1.1/test/test.pep > ./netMHCcons/netMHCcons-1.1/test/test.pep_userMHC.myout.PickPocket
+
+
+
+
